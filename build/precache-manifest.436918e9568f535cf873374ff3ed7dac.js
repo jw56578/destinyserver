7cd491711c77c30d63daab7719abf0f80c92f02d
Jon Woo <jw56578@gmail.com>
2020-11-18 11:41:47 -0600

    add build

---
+++ build/precache-manifest.436918e9568f535cf873374ff3ed7dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58caa0236e76dbe2efe6a523a587eb5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "639da0590fabb211acf0",
+    "url": "/static/css/main.a1f3e13c.chunk.css"
+  },
+  {
+    "revision": "1aca27c44fb8ed468bf8",
+    "url": "/static/js/2.ea93de27.chunk.js"
+  },
+  {
+    "revision": "e72aa5db2c8ada42b45ae4ae98923678",
+    "url": "/static/js/2.ea93de27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639da0590fabb211acf0",
+    "url": "/static/js/main.488ac778.chunk.js"
+  },
+  {
+    "revision": "80d5dc00158f2545199a",
+    "url": "/static/js/runtime-main.9a94cc45.js"
+  }
+]);
